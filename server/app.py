996d2d873a2bdd3fa17a8d14150ab9b3c57ebc21
neville777 <neville.james@student.moringaschool.com>
2024-01-28 06:25:38 +0300

    seeding the db

--- server/app.py
+++ server/app.py
@@ -31,6 +31,5 @@ with app.app_context():
     db.create_all()
     
 
-
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True, port=5000 )

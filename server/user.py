b0504f30e070a275bb4ed9f9b9da3a3e5f2f90b1
neville777 <neville.james@student.moringaschool.com>
2024-01-28 12:38:25 +0300

    the user component

---
+++ server/user.py
@@ -0,0 +1,75 @@
+from flask import request
+from flask_restful import Resource
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
+from model import db, User, bcrypt
+
+class UserRegistrationResource(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            username = data.get('username')
+            password = data.get('password')
+            
+            if not username or not password:
+                return {'error': 'Please provide username and password'}, 400
+            
+            existing_user = User.query.filter_by(username=username).first()
+            if existing_user:
+                return {'error': 'Username is already taken'}, 400
+            
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            new_user = User(username=username, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            
+            access_token = create_access_token(identity=username)
+            refresh_token = create_refresh_token(identity=username)
+            
+            
+            return {'message': 'User registered succesfully.',
+                    'access_token': access_token,
+                    'refresh_token': refresh_token
+            },201
+            
+        except Exception as e:
+            return {'error': str(e)},500
+
+class UserLoginResource(Resource):
+   def post(self):
+        try:
+            data = request.get_json()
+            username = data.get('username')
+            password = data.get('password')
+
+            user = User.query.filter_by(username=username).first()
+            if user and bcrypt.check_password_hash(user.password, password):
+                access_token = create_access_token(identity=username)
+                refresh_token = create_refresh_token(identity=username)
+                return {"access_token": access_token, "refresh_token": refresh_token}, 200
+            else:
+                return {"error": "Invalid username or password."}, 401
+        except Exception as e:
+            return {"error": str(e)}, 500
+class RefreshTokenResource(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user)
+        return {"access_token": access_token}, 200
+class UserResource(Resource):
+    @jwt_required()
+    def get(self):
+        try:
+            current_user = get_jwt_identity()
+            user = User.query.filter_by(username=current_user).first()
+
+            if user:
+                return {
+                    "id": user.id,
+                    "username": user.username,
+                    "role": user.role,
+                }, 200
+            else:
+                return {"message": "User not found."}, 404
+        except Exception as e:
+            return {"error": str(e)}, 500
